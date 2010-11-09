aca47a71f4b7ef2b5541caf81ee8df2921f6d261
Antonio Pardo <antonio.pardo@gmail.com>
2010-11-09 10:34:55 +0100

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sahara::Application.config.secret_token = '774e167fef73894c69d69455b878c325945427283f7a63f91d951541bfa3a4deb754a6812d96cbd9fd82cf59eb978eb894d80b7cfb5103f5179e998d64d51b54'
