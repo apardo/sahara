b946c8582db251bed3b0e7cb36fb02ffd79f6956
Antonio Pardo <antonio.pardo@gmail.com>
2010-11-10 02:38:05 +0100

    rutas y conf.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Sahara::Application.routes.draw do
+  
+  get "message/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +52,7 @@ Sahara::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => 'site#index'
 
   # See how all your routes lay out with "rake routes"
 
