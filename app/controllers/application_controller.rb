4e6b50e1d3a88410c98b7afdee2b2cdb240abef7
Antonio Pardo <antonio.pardo@gmail.com>
2010-11-10 02:38:40 +0100

    controlador base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_locale
+
+  private
+
+  def set_locale
+    if params[:locale]
+      I18n.locale = params[:locale]
+      cookies[:locale] = { :value => I18n.locale, :expires => 10.years.from_now }
+    else
+      if cookies[:locale]
+        I18n.locale = cookies[:locale]
+      else
+        if extract_locale_from_accept_language_header == 'en'
+          I18n.locale = 'en'
+        else
+          I18n.locale = 'es'
+        end
+        cookies[:locale] = { :value => I18n.locale, :expires => 10.years.from_now }
+      end
+    end
+  end
+
+  def extract_locale_from_accept_language_header
+    if request.env['HTTP_ACCEPT_LANGUAGE']
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
+  end
 end
